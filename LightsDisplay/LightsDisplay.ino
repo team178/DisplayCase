294316ac29844f4f573f9e01ef1cafb26a94a975
Brandon Demkowicz <20demkowiczbr@fpsct.org>
2017-10-03 20:30:31 -0400

    added method that makes lights show Enforcers colors

--- LightsDisplay/LightsDisplay.ino
+++ LightsDisplay/LightsDisplay.ino
@@ -13,3 +13,21 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void enforcersColors()
+{
+  for (int i = 1; i<=48;i++)
+{
+  if ((i%2)==0)
+  {
+    strip.setPixelColor(i,78, 66, 244);
+  }
+  else if ((i%2) == 1)
+  {
+    strip.setPixelColor(i,244, 238, 66);
+  }
+  }
+strip.show();
+delay(100);
+}
+
