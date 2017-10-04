698d6a49aeaab0bf37e70afa12520736a12c4c2f
Brandon Demkowicz <20demkowiczbr@fpsct.org>
2017-10-03 20:19:14 -0400

    Code imports Wire and Adafruit_NeoPixel libraries, as well as constructs an object for the LEDS

--- LightsDisplay/LightsDisplay.ino
+++ LightsDisplay/LightsDisplay.ino
@@ -1,3 +1,9 @@
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+
+//Makes strip object- don't know how many LEDS there are yet, so I guessed 48?
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(48, 3, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   // put your setup code here, to run once:
 
